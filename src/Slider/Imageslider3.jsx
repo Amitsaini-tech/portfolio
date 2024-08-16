13bd711a7edbad90f7fc62d8cdc514787a2a1760
Amitsaini-tech <sainiamit3464@gmail.com>
2024-08-16 16:34:29 +0530

    increase top

--- src/Slider/Imageslider3.jsx
+++ src/Slider/Imageslider3.jsx
@@ -84,7 +84,7 @@ const MediaCarousel = () => {
             </div>
           ))}
           <button
-            className="absolute top-4 right-4 text-white bg-gray-800 px-4 py-2 rounded"
+            className="absolute top-5 right-4 text-white bg-gray-800 px-4 py-2 rounded"
             onClick={handleCloseGallery}
           >
             Close
